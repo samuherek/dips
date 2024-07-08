191ac64061b92160c2f072311440d9e4be17e368
Sam Uherek <samuherekbiz@gmail.com>
2024-07-08 13:33:10 +0200

    setup the crates to get started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { input: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Some(Commands::Add { input }) => {
+            println!("we are adding {}", input);
+            let dir = std::env::current_dir().expect("Failed to find current directory");
+            println!(" curr directory is {:?}", dir);
+        }
+        None => {}
+    }
 }
